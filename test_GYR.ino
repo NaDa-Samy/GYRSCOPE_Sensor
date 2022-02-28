1d7b74e2ae9692e39608649d6d928ce6850569b2
NaDa <82848600+NaDa-Samy@users.noreply.github.com>
2022-02-28 02:45:32 +0200

    Add files via upload

---
+++ test_GYR.ino
@@ -0,0 +1,31 @@
+#include<Wire.h>
+const int MPU=0x68; 
+int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
+
+void setup(){
+  Wire.begin();
+  Wire.beginTransmission(MPU);
+  Wire.write(0x6B); 
+  Wire.write(0);    
+  Wire.endTransmission(true);
+  Serial.begin(9600);
+}
+void loop(){
+  Wire.beginTransmission(MPU);
+  Wire.write(0x3B);  
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU,12,true);  
+  AcX=Wire.read()<<8|Wire.read();    
+  AcY=Wire.read()<<8|Wire.read();  
+  AcZ=Wire.read()<<8|Wire.read();  
+  GyX=Wire.read()<<8|Wire.read();  
+  GyY=Wire.read()<<8|Wire.read();  
+  GyZ=Wire.read()<<8|Wire.read();  
+  
+  Serial.print("Gyroscope: ");
+  Serial.print(" Horizontal = "); Serial.print(GyX);
+  Serial.print(" < Vertical = "); Serial.print(GyY);
+  Serial.print(" < Height   = "); Serial.println(GyZ);
+  Serial.println(" ");
+  delay(2000);
+}
